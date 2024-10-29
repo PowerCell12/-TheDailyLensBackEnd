439f41c82522dcc36f1437b408254ebb7aac2d65
PowerCell12 <gumbal4e@gmail.com>
2024-10-30 00:54:46 +0200

    adding gitignore

--- obj/Debug/net8.0/server.AssemblyInfo.cs
+++ obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5021508ba1a003d92a34e3a4fe73a7cdf6dd4741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c8c63192c0c6197f6e9757eb925b6dd603125d")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
